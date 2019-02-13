d9aaf0bc864f8ed4d9455076880ce73860ff88ea
David Tan <davidtan@thoughtworks.com>
2019-02-13 13:34:08 +0800

    Change name of heroku auth env variable

--- bin/heroku/deploy.sh
+++ bin/heroku/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 heroku_app_name=$1
-heroku_repo="https://heroku:$HEROKU_APP_KEY@git.heroku.com/$heroku_app_name.git"
+heroku_repo="https://heroku:$HEROKU_AUTH_TOKEN@git.heroku.com/$heroku_app_name.git"
 
 git push $heroku_repo master
