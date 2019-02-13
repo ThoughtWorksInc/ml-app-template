a968c5f482e7261df70dad6d61fd3cdefef00178
David Tan <davidtan@thoughtworks.com>
2019-02-13 11:53:55 +0800

    Track training parameters using mlflow

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import joblib
 
+import mlflow
 from sklearn import datasets
 import numpy as np
 import pandas as pd
@@ -11,8 +12,17 @@ x = pd.DataFrame(boston.data, columns=boston.feature_names)
 y = pd.DataFrame(boston.target, columns=["MEDV"])
 column_order = x.columns
 
-lm = linear_model.LinearRegression()
-model = lm.fit(x, y)
+uri = 'http://35.240.197.5:5000'
+mlflow.set_tracking_uri(uri)
+mlflow.set_experiment('dev')
+
+with mlflow.start_run() as run:
+  lm = linear_model.LinearRegression()
+  model = lm.fit(x, y)
+
+  mlflow.log_param("my", "param")
+  mlflow.log_metric("rmse", 0.5)
+
 
 joblib.dump(model, 'models/linear_model.joblib') 
-joblib.dump(column_order, 'models/column_order.joblib') 
+joblib.dump(column_order, 'models/column_order.joblib')
