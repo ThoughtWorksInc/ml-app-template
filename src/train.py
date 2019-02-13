058c79502727f7f7b7bd9439bac6c6a775b3e28f
davified <davified@gmail.com>
2019-02-13 07:47:13 +0000

    Log r2 and rmse to mlflow when running train.py

--- src/train.py
+++ src/train.py
@@ -1,26 +1,39 @@
+import os
 import joblib
-
 import mlflow
-from sklearn import datasets
+from math import sqrt
 import numpy as np
 import pandas as pd
+from sklearn import datasets
 from sklearn import linear_model
+from sklearn import metrics 
+from sklearn.model_selection import train_test_split
 
+
+# load data
 boston = datasets.load_boston()
 
+# preprocess data
 x = pd.DataFrame(boston.data, columns=boston.feature_names)
-y = pd.DataFrame(boston.target, columns=["MEDV"])
 column_order = x.columns
+y = pd.DataFrame(boston.target, columns=["MEDV"])
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
+# configure mlflow
 mlflow.set_tracking_uri(uri='http://35.240.197.5:5000')
-mlflow.set_experiment('dev')
+if os.environ.get('CI', '') == 'true':
+  mlflow.set_experiment('CI')
+else:
+  mlflow.set_experiment('dev')
 
 with mlflow.start_run() as run:
   lm = linear_model.LinearRegression()
-  model = lm.fit(x, y)
+  model = lm.fit(x_train, y_train)
 
-  mlflow.log_param("my", "param")
-  mlflow.log_metric("rmse", 0.5)
+  y_test_pred = model.predict(x_test)
+  
+  mlflow.log_metric("rmse", sqrt(metrics.mean_squared_error(y_true=y_test, y_pred=y_test_pred)))
+  mlflow.log_metric("r2_score", metrics.r2_score(y_true=y_test, y_pred=y_test_pred))
 
 joblib.dump(model, 'models/linear_model.joblib') 
 joblib.dump(column_order, 'models/column_order.joblib')
