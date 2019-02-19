24ecf4420ff8e323957a09cdf71176839fb6adff
David Tan <davidtan@thoughtworks.com>
2019-02-19 16:09:24 +0800

    Add echo statement when deploying

--- bin/deploy_to_heroku.sh
+++ bin/deploy_to_heroku.sh
@@ -4,6 +4,7 @@ set -e
 heroku_app_name=$1
 heroku_repo="https://heroku:$HEROKU_AUTH_TOKEN@git.heroku.com/$heroku_app_name.git"
 
+echo "Deploying app to heroku..."
 git push $heroku_repo master
 
 echo "Running smoke test"
