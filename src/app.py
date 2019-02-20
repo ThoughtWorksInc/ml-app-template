ef21c31fb38796b4aa3383e86b0fab890f85452c
David Tan <davidtan@thoughtworks.com>
2019-02-20 08:46:23 +0800

    Revert "Simulating bad build"
    This reverts commit 24cd645725b33cf6f77ade589187156363dafb61.

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ model = joblib.load('models/model.joblib')
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return jsonify({"response": "bad build!"})
+    return jsonify({"response": "hello world!"})
 
 @app.route('/predict', methods=['POST'])
 def predict():
