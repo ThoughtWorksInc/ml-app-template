bf64de12d6b6b3eee92cd712b59df30469e3d316
David Tan <davidtan@thoughtworks.com>
2019-02-20 08:44:02 +0800

    Allow deploy to heroku from a previous commit

--- bin/deploy_to_heroku.sh
+++ bin/deploy_to_heroku.sh
@@ -5,7 +5,7 @@ heroku_app_name=$1
 heroku_repo="https://heroku:$HEROKU_AUTH_TOKEN@git.heroku.com/$heroku_app_name.git"
 
 echo "Deploying app to heroku..."
-git push $heroku_repo master
+git push $heroku_repo master --force
 
 echo "Running smoke test"
 bin/predict.sh "$heroku_app_name.herokuapp.com"
