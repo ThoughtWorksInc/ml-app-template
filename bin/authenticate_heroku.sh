922fcfeaaeabeb29ef7423594bf0fe60fa5d7bda
David Tan <davidtan@thoughtworks.com>
2019-02-23 23:02:45 +0800

    Release docker image using heroku-cli

--- bin/authenticate_heroku.sh
+++ bin/authenticate_heroku.sh
@@ -4,8 +4,8 @@ set -e
 HEROKU_AUTH_TOKEN=$1
 
 IFS=''
-echo 'machine api.heroku.com' >> .netrc
-echo '  login davified@gmail.com' >> .netrc
-echo "  password $HEROKU_AUTH_TOKEN" >> .netrc
+echo 'machine api.heroku.com' >> ~/.netrc
+echo '  login davified@gmail.com' >> ~/.netrc
+echo "  password $HEROKU_AUTH_TOKEN" >> ~/.netrc
   
   
