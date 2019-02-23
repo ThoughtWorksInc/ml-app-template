5f163df185b68fbc1332460accb75d9e20df7c31
David Tan <davidtan@thoughtworks.com>
2019-02-23 22:49:45 +0800

    Make heroku deployment script executable

--- bin/deploy_heroku_container.sh
+++ bin/deploy_heroku_container.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-HEROKU_APP_NAME=$1
+heroku_app_name=$1
+container_name='david-docker-staging'
 
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
-  -d '{
+curl -V -n -X PATCH https://pi.heroku.com/apps/$heroku_app_name/formation \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  --data @<(cat <<EOF
+{
   "updates": [
     {
       "type": "web",
-      "docker_image": "registry.heroku.com/david-docker-staging/web"
+      "docker_image": "registry.heroku.com/$container_name/web"
     },
   ]
-}' \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+}
+EOF
+)
+  -d '' \
