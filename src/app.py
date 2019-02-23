9a31813caaabb979bb962cfacc6f83e212bbc5b0
David Tan <davidtan@thoughtworks.com>
2019-02-23 23:34:05 +0800

    Revert to hello world endpoint

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ model = joblib.load('models/model.joblib')
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return jsonify({"response": "hello docker on heroku!"})
+    return jsonify({"response": "hello world!"})
 
 @app.route('/predict', methods=['POST'])
 def predict():
