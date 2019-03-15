3676c5b3d42aebbb594d29388d95d0f9b44a4db4
David Tan <davidtan@thoughtworks.com>
2019-03-15 09:32:11 +0800

    Add fluentd logging client

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-import os, logging
+import os, json
 
 import joblib
 import pandas as pd
 from flask import Flask, jsonify, request
+from fluent import sender
+from fluent import event
 
 app = Flask(__name__)
 column_order = joblib.load('models/column_order.joblib') 
@@ -10,8 +12,6 @@ model = joblib.load('models/model.joblib')
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    print('hello world!!')
-    logging.log(logging.DEBUG, 'hello world!!!')
     return jsonify({"response": "hello world!"})
 
 @app.route('/predict', methods=['POST'])
@@ -21,7 +21,17 @@ def predict():
     input_features = input_features.append(request_payload, ignore_index=True)
     input_features = input_features.fillna(0)
 
-    prediction = model.predict(input_features.values.tolist()).tolist()
+    prediction = model.predict(input_features.values.tolist()).tolist()[0]
+
+    logger = sender.FluentSender('app', host='host.docker.internal', port=24224)
+    feature_names = column_order.tolist()
+    feature_values = input_features.values.tolist()[0]
+    log_payload = {'prediction': prediction, **dict(zip(feature_names, feature_values))}
+    if not logger.emit('prediction', log_payload):
+        print('logger error')
+        print(logger.last_error)
+        logger.clear_last_error() # clear stored error after handled errors
+
     return jsonify({'predicted price (thousands)': prediction})
 
 if __name__ == '__main__':    
